30da8198dd2f2b208ca4153970354fd732dec67f
dhiasalah <med.dhia.sala@gmail.com>
2026-01-29 22:31:12 +0100

    removing old frontend

--- frontend/src/hooks/index.ts
+++
@@ -1,7 +0,0 @@
-// Custom hooks for the Web Audio Sampler application
-
-export { useAudioEngine } from "./useAudioEngine";
-export { useKeyboardControls, keyToPadMap } from "./useKeyboardControls";
-export { usePresetLoader } from "./usePresetLoader";
-export { usePadSelection } from "./usePadSelection";
-export { useRecording } from "./useRecording";
